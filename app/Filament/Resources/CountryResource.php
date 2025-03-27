bc267cfe8453b87d2a64df2e04d0fdfc8bd8de6d
nurmuhammad701 <nurmuhammadmel@gmail.com>
2025-03-27 15:05:17 +0500

    resources are changed

--- app/Filament/Resources/CountryResource.php
+++ app/Filament/Resources/CountryResource.php
@@ -27,6 +27,8 @@ class CountryResource extends Resource
 
     protected static ?string $slug = 'employees-country';
 
+    protected static ?int $navigationSort = 1;
+
     public static function form(Form $form): Form
     {
         return $form
